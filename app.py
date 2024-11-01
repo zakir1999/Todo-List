b5cbe5e8a5da9a00372127c9b195d5bce15c74dc
zakir1999 <m.zakirhossen99@gmail.com>
2024-11-02 03:29:18 +0600

    Second Commit

--- app.py
+++ app.py
@@ -1,14 +1,54 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,request
 from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
+
+
 app=Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"]="sqlite:///todo.db"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"]=False
-dp=SQLAlchemy(app)
+db=SQLAlchemy(app)
 
-@app.route('/')
+class Todo(db.Model):
+    sno=db.Column(db.Integer,primary_key=True)
+    title =db.Column(db.String(200),nullable=False)
+    desc=db.Column(db.String(500),nullable=False)
+    date_created=db.Column(db.DateTime,default=datetime.utcnow)
+
+
+    def __repr__(self)->str:
+        return f"{self.sno} - {self.title}"
+
+@app.route('/',methods=['GET','POST'])
 def hell_world():
-    return render_template('index.html')
+    if request.method=="POST":
+        title=request.form['title']
+        desc=request.form['desc']
+        todo=Todo(title=title,desc=desc)
+        db.session.add(todo)
+        db.session.commit()
 
+    allTodo=Todo.query.all()
+    return render_template('index.html',allTodo=allTodo)
+
+@app.route('/update')
+def update():
+    allTodo=Todo.query.all()
+    print(allTodo)
+    return 'This is product page'
+
+
+@app.route('/delete/<int:sno>')
+def delete(sno):
+    allTodo=Todo.query.all()
+    print(allTodo)
+    return 'This is product page'
+
+@app.route('/show')
+def products():
+    allTodo=Todo.query.all()
+    print(allTodo)
+    return 'This is product page'
 
 if __name__=="__main__":
     app.run(debug=True)
